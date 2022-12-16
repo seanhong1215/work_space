af3ff6a16333e8a5ec6097abbf000932145c9409
seanhong1215 <seanhong1215@gmail.com>
2022-12-17 01:09:38 +0800

    config edit

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,2 +1,2 @@
-const BASE_URL = "http://localhost:3000";
-// const BASE_URL = "https://json-server-vercel-one.vercel.app";
+// const BASE_URL = "http://localhost:3000";
+const BASE_URL = "https://json-server-vercel-one.vercel.app";
